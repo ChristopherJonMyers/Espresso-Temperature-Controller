f3572ad3b7e21bf0ac27f8bc38ffe8de6a1ae2ce
Chris Myers <36175788+ChristopherJonMyers@users.noreply.github.com>
2021-09-22 13:35:11 -0400

    Added app.py for flask wep app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import time
+import board
+import digitalio
+import adafruit_max31856
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    curTemp = Tcup()
+    brewTemp = 94
+    return render_template('index.html', curTemp=curTemp, brewTemp=brewTemp)
+
+def Tcup():
+    spi = board.SPI()
+    cs = digitalio.DigitalInOut(board.D5)
+    cs.direction = digitalio.Direction.OUTPUT
+    thermocouple = adafruit_max31856.MAX31856(spi, cs)
+    curTemp = thermocouple.temperature
+    if curTemp is not None:
+        return round(curTemp,1)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
